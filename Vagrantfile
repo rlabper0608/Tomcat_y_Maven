b3207dd4e1abccb989256a95779aee39a7e92d55
Raúl-Labrador <raullabrador10@gmail.com>
2026-01-08 20:24:11 +0100

    Actualización de archivos

--- Vagrantfile
+++ Vagrantfile
@@ -5,10 +5,13 @@ Vagrant.configure("2") do |config|
 
   config.vm.synced_folder ".", "/vagrant"
 
-  config.vm.provision "shell", path: "bootstrap.sh"
-end
-
-config.vm.provider "virtualbox" do |vb|
+  config.vm.provider "virtualbox" do |vb|
   vb.memory = "2048"
   vb.cpus = 2
+  # Esto evita bloqueos en procesos de red/seguridad
+  vb.customize ["modifyvm", :id, "--hwvirtex", "on"]
+  vb.customize ["modifyvm", :id, "--ioapic", "on"]
+  end
+
+  config.vm.provision "shell", path: "bootstrap.sh"
 end
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
+export DEBIAN_FRONTEND=noninteractive
 
 set -xeu
 apt-get update -y
-apt-get upgrade -y
+# Instalar herramientas que aceleran el resto del proceso
+apt-get install -y zstd curl wget
 
-# sudo apt install -y openjdk-11-jdk
-sudo apt-get install -y openjdk-11-jdk
-sudo apt install -y tomcat9
+echo "deb http://deb.debian.org/debian bullseye main" | sudo tee /etc/apt/sources.list.d/bullseye.list
+apt-get update -y
+apt-get install -y openjdk-11-jdk
+apt install -y tomcat9 tomcat9-admin
 
-sudo groupadd tomcat9
-sudo useradd -s /bin/false -g tomcat9 -d /etc/tomcat9 tomcat9
+groupadd tomcat9
+useradd -s /bin/false -g tomcat9 -d /etc/tomcat9 tomcat9
 
-sudo systemctl start tomcat9
-sudo systemctl status tomcat9
+cp /vagrant/context.xml /usr/share/tomcat9-admin/host-manager/META-INF/context.html
+cp /vagrant/tomcat-users.xml /etc/tomcat9/tomcat-users.xml
+
+apt-get update && sudo apt-get -y install maven
+
+systemctl start tomcat9
+systemctl status tomcat9
+systemctl restart tomcat9
+
+mvn --v
